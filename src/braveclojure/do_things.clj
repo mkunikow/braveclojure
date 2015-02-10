e444029b32eed736b5b8e8b3faded23d7e865cdd
mkunikowski <michal.kunikowski@gmail.com>
2015-02-10 20:30:22 +0100

    chapter3 do_things

--- src/braveclojure/do_things.clj
+++ src/braveclojure/do_things.clj
@@ -685,7 +685,7 @@ failed-protagonist-names
     ;     (conj (conj final-body-parts (first remaining-asym-parts)))))
 
 
-;4.3. loop
+;; 4.3. loop
 
 (loop [iteration 0]
   (println (str "Iteration " iteration))
@@ -720,7 +720,7 @@ failed-protagonist-names
 ;Iteration 4
 ;Goodbye!
 
-;4.4. Regular Expressions
+;; 4.4. Regular Expressions
 
 ;; pound, open quote, close quote
 #"regular-expression"
@@ -742,3 +742,100 @@ failed-protagonist-names
 
 (make-matching-part {:name "left-eye" :size 1})
 ; => {:name "right-eye" :size 1}]
+
+;; 4.5. Symmetrizer
+
+;def asym-hobbit-body-parts [{:name "head" :size 3}
+;                            {:name "left-eye" :size 1}
+;                            {:name "left-ear" :size 1}
+;                            {:name "mouth" :size 1}
+;                            {:name "nose" :size 1}
+;                            {:name "neck" :size 2}
+;                            {:name "left-shoulder" :size 3}
+;                            {:name "left-upper-arm" :size 3}
+;                            {:name "chest" :size 10}
+;                            {:name "back" :size 10}
+;                            {:name "left-forearm" :size 3}
+;                            {:name "abdomen" :size 6}
+;                            {:name "left-kidney" :size 1}
+;                            {:name "left-hand" :size 2}
+;                            {:name "left-knee" :size 2}
+;                            {:name "left-thigh" :size 4}
+;                            {:name "left-lower-leg" :size 3}
+;                            {:name "left-achilles" :size 1}
+;                            {:name "left-foot" :size 2}]
+
+;(defn needs-matching-part?
+;  [part]
+;  (re-find #"^left-" (:name part)))
+;
+;(defn make-matching-part
+;  [part]
+;  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+;   :size (:size part)})
+
+; ~~~1~~~
+;(defn symmetrize-body-parts
+;  "Expects a seq of maps which have a :name and :size"
+;  [asym-body-parts] ;
+;  (loop [remaining-asym-parts asym-body-parts ; ~~~2~~~
+;         final-body-parts []]
+;    (if (empty? remaining-asym-parts) ; ~~~3~~~
+;      final-body-parts
+;      (let [[part & remaining] remaining-asym-parts ; ~~~4~~~
+;            final-body-parts (conj final-body-parts part)]
+;        (if (needs-matching-part? part) ; ~~~5~~~
+;          (recur remaining (conj final-body-parts (make-matching-part part))) ; ~~~6~~~
+;          (recur remaining final-body-parts))))))
+
+;; 4.6. Shorter Symmetrizer with Reduce
+
+;; sum with reduce
+(reduce + [1 2 3 4])
+; => 10
+
+(reduce + 15 [1 2 3 4])
+
+(defn my-reduce
+  ([f initial coll]
+    (loop [result initial
+           remaining coll]
+      (let [[current & rest] remaining]
+        (if (empty? remaining)
+          result
+          (recur (f result current) rest)))))
+  ([f [head & tail]]
+    (my-reduce f (f head (first tail)) (rest tail))))
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps which have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (let [final-body-parts (conj final-body-parts part)]
+              (if (needs-matching-part? part)
+                (conj final-body-parts (make-matching-part part))
+                final-body-parts)))
+          []
+          asym-body-parts))
+
+
+;; 4.7. Hobbit Violence
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + 0 (map :size sym-parts))
+        target (inc (rand body-part-size-sum))]
+    (loop [[part & rest] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur rest (+ accumulated-size (:size part)))))))
+
+(hit asym-hobbit-body-parts)
+; => {:name "right-upper-arm", :size 3}
+
+(hit asym-hobbit-body-parts)
+; => {:name "chest", :size 10}
+
+(hit asym-hobbit-body-parts)
+; => {:name "left-eye", :size 1}
