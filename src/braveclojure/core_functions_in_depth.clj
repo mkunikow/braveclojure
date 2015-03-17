6bc69e3a392f7b22b8b50c848e8a885ea7fffe15
mkunikowski <michal.kunikowski@gmail.com>
2015-03-17 22:34:56 +0100

    chapter3 do_things

--- src/braveclojure/core_functions_in_depth.clj
+++ src/braveclojure/core_functions_in_depth.clj
@@ -88,3 +88,96 @@
 
 (stats [80 1 44 13 6])
 ; => (144 5 144/5)
+
+;2.2.2. reduce
+
+(reduce (fn [new-map [key val]]
+          (assoc new-map key (inc val)))
+        {}
+        {:max 30 :min 10})
+; => {:max 31, :min 11}
+
+(reduce (fn [new-map [key val]]
+          (if (> val 4)
+            (assoc new-map key val)
+            new-map))
+        {}
+        {:human 4.1
+         :critter 3.9})
+; {:human 4.1}
+
+;2.2.3. take, drop, take-while, drop-while
+(take 3 [1 2 3 4 5 6 7 8 9 10])
+; => (1 2 3)
+
+(drop 3 [1 2 3 4 5 6 7 8 9 10])
+; => (4 5 6 7 8 9 10)
+
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+; => ({:month 1 :day 1 :human 5.3 :critter 2.3}
+;{:month 1 :day 2 :human 5.1 :critter 2.0}
+;{:month 2 :day 1 :human 4.9 :critter 2.1}
+;{:month 2 :day 2 :human 5.0 :critter 2.5})
+
+(drop-while #(< (:month %) 3) food-journal)
+; => ({:month 3 :day 1 :human 4.2 :critter 3.3}
+;{:month 3 :day 2 :human 4.0 :critter 3.8}
+;{:month 4 :day 1 :human 3.7 :critter 3.9}
+;{:month 4 :day 2 :human 3.7 :critter 3.6})
+
+
+(take-while #(< (:month %) 4)
+            (drop-while #(< (:month %) 2) food-journal))
+; => ({:month 2 :day 1 :human 4.9 :critter 2.1}
+;{:month 2 :day 2 :human 5.0 :critter 2.5}
+;{:month 3 :day 1 :human 4.2 :critter 3.3}
+;{:month 3 :day 2 :human 4.0 :critter 3.8})
+
+;2.2.4. filter, some
+
+(filter #(< (:human %) 5) food-journal)
+; => ({:month 2 :day 1 :human 4.9 :critter 2.1}
+;{:month 3 :day 1 :human 4.2 :critter 3.3}
+;{:month 3 :day 2 :human 4.0 :critter 3.8}
+;{:month 4 :day 1 :human 3.7 :critter 3.9}
+;{:month 4 :day 2 :human 3.7 :critter 3.6})
+
+
+(filter #(< (:month %) 3) food-journal)
+; => ({:month 1 :day 1 :human 5.3 :critter 2.3}
+;{:month 1 :day 2 :human 5.1 :critter 2.0}
+;{:month 2 :day 1 :human 4.9 :critter 2.1}
+;{:month 2 :day 2 :human 5.0 :critter 2.5})
+
+
+(some #(> (:critter %) 5) food-journal)
+; => nil
+
+(some #(> (:critter %) 3) food-journal)
+; => true
+
+(some #(and (> (:critter %) 3) %) food-journal)
+; => {:month 3 :day 1 :human 4.2 :critter 3.3}
+
+
+;2.2.5. sort, sort-by
+(sort [3 1 2])
+; => (1 2 3)
+
+(sort-by count ["aaa" "c" "bb"])
+; => ("c" "bb" "aaa")
+
+;2.2.6. concat
+(concat [1 2] [3 4])
+; => (1 2 3 4)
