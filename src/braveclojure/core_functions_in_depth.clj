e1582f1d7a927d69b318dd986243fe0928b9f11e
Michal Kunikowski <michal.kunikowski@gtech.com>
2015-03-17 16:56:30 +0100

    core functions

--- src/braveclojure/core_functions_in_depth.clj
+++ src/braveclojure/core_functions_in_depth.clj
@@ -40,3 +40,51 @@
 
 
 ;1.2. Distinguishing Abstraction and Implementation
+
+;2. The Sequence Abstraction
+;2.1. Seq Functions Convert Data Structures to Seqs
+
+(identity "Stefan Salvatore from Vampire Diaries")
+; => "Stefan Salvatore from Vampire Diaries"
+
+(map identity {:name "Bill Compton" :occupation "Dead mopey guy"})
+; => ([:name "Bill Compton"] [:occupation "Dead mopey guy"])
+
+(seq {:name "Bill Compton" :occupation "Dead mopey guy"})
+; => ([:name "Bill Compton"] [:occupation "Dead mopey guy"])
+
+
+; 2.2. Seq Function Examples
+; 2.2.1. map
+
+(map inc [1 2 3])
+; => (2 3 4)
+
+(map str ["a" "b" "c"] ["A" "B" "C"]) ; == (list (str "a" "A") (str "b" "B") (str "c" "C"))
+; => ("aA" "bB" "cC")
+
+(def human-consumption   [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
+
+(map unify-diet-data human-consumption critter-consumption)
+; => ({:human 8.1, :critter 0.0}
+;{:human 7.3, :critter 0.2}
+;{:human 6.6, :critter 0.3}
+;{:human 5.0, :critter 1.1})
+
+
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+
+(stats [3 4 10])
+; => (17 3 17/3)
+
+(stats [80 1 44 13 6])
+; => (144 5 144/5)
